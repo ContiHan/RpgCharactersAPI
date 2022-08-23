c8628b177da4ea96860e0928b1cabb9f8b7630bb
ContiHan <daniel.hanak@continental-corporation.com>
2022-08-23 14:28:07 +0200

    add users as table to DB

--- Relationships/Data/DataContext.cs
+++ Relationships/Data/DataContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Relationships.Models;
 
 namespace Relationships.Data
 {
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
     }
 }
