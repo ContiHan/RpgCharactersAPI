78dad8c0191e8b09fd507a819ead39efdb247533
ContiHan <daniel.hanak@continental-corporation.com>
2022-08-23 16:19:17 +0200

    get characters by user id

---
+++ Relationships/Controllers/CharacterController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Relationships.Data;
+using Relationships.Models;
+
+namespace Relationships.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CharacterController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = nameof(GetAllCharacters))]
+        public async Task<ActionResult<IEnumerable<Character>>> GetAllCharacters()
+        {
+            if (_context.Characters is null)
+            {
+                return NotFound();
+            }
+            return Ok(await _context.Characters.Include(i => i.User).ToListAsync());
+        }
+
+        [HttpGet("userId={userId:int}", Name = nameof(GetCharactersByUserId))]
+        public async Task<ActionResult<Character>> GetCharactersByUserId(int userId)
+        {
+            var characters = _context.Characters
+                .Include(i => i.User)
+                .Where(c => c.UserId == userId);
+            if (characters is null || !await characters.AnyAsync())
+            {
+                return NotFound();
+            }
+            return Ok(await characters.ToListAsync());
+        }
+    }
+}
--- Relationships/Models/User.cs
+++ Relationships/Models/User.cs
@@ -1,9 +1,13 @@
-﻿namespace Relationships.Models
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Relationships.Models
 {
     public class User
     {
         public int Id { get; set; }
         public string Username { get; set; } = string.Empty;
+        [JsonIgnore]
         public List<Character> Characters { get; set; }
     }
 }
